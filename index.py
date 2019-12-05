adcd0a2c2db06b6341ee81feb240f2d07f0ed833
Jibram <cajibram@gmail.com>
2019-12-04 16:16:26 -0800

    Finished main functionality backend endpoints

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import sqlite3
 import json
 
@@ -8,6 +8,15 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello_world():
     return 'Hello World'
+
+@app.route('/authorizelogin')
+def authorize():
+    with sqlite3.connect('KVDB.db') as conn:
+        userID = request.args.get('userID')
+        select_query = "SELECT password FROM users WHERE userID='" + userID + "'"
+        db_cursor = conn.cursor()
+        db_cursor.execute(select_query)
+        return db_cursor.fetchone()[0]
 
 @app.route('/getalldata')
 def get_patients():
@@ -26,11 +35,27 @@ def get_patients():
         # Get results
         return res
 
-@app.route('/getpatient')
-def get_patient(patientID):
+@app.route('/getpatientbyid')
+def get_patient():
     with sqlite3.connect('KVDB.db') as conn:
+        patientID = request.args.get('patientID')
         # Construct the query
         select_query = "SELECT * FROM patients WHERE patientID=" + patientID
+        
+        # Get pointer to db
+        db_cursor = conn.cursor()
+
+        # Pass the query to DB
+        db_cursor.execute(select_query)
+
+        return json.dumps(db_cursor.fetchone())
+
+@app.route('/getpatientbyname')
+def get_patientByName():
+    with sqlite3.connect('KVDB.db') as conn:
+        patientName = request.args.get('patientName')
+        # Construct the query
+        select_query = "SELECT * FROM patients WHERE patientName='" + patientName + "'"
 
         # Get pointer to db
         db_cursor = conn.cursor()
@@ -38,24 +63,23 @@ def get_patient(patientID):
         # Pass the query to DB
         db_cursor.execute(select_query)
 
-        res = db_cursor.fetchone()
-
-        # Get results
-        return json.dumps(res)
+        return json.dumps(db_cursor.fetchone())
 
 @app.route('/insertpatient')
-def post_patient(patientName, parentName, phoneNumber, meData, aeData):
+def post_patient():
     with sqlite3.connect('KVDB.db') as conn:
-        insert_query = "INSERT INTO patients(patientName, parentName, phoneNumber, meData, aeData) VALUES(" + patientName + parentName + phoneNumber + meData + aeData + ")"
-        
+        patientName = request.args.get('patientName')
+        parentName = request.args.get('parentName')
+        phoneNumber = request.args.get('phoneNumber')
+        aeData = request.args.get('aeData')
+        meData = request.args.get('meData')
+
+        insert_query = "INSERT INTO patients(patientName, parentName, phoneNumber, aeData, meData) VALUES('" + patientName + "', '" +  parentName + "', " + phoneNumber + ", " + aeData + ", '" + meData + "')"
+        print(insert_query)
         db_cursor = conn.cursor()
 
-        db_cursor.execute(insert_query)
-
-        res = db_cursor.fetchall()
-
-        # Get results
-        return res
-
+        db_cursor.execute(insert_query)  
+        print(json.dumps(db_cursor.fetchone()))
+        return "OK"
 if __name__ == '__main__':
     app.run(port=5000)
