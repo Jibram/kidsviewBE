e5d579911cf2b28a8154e942bcc86c88ab0a90d4
Raj Yuvaraj <ryuvaraj37@gmail.com>
2019-12-05 17:32:30 -0800

    Added flash cors which allows for ports on the same comptuer to interact

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
 from flask import Flask, request
+from flask_cors import CORS
 import sqlite3
 import json
 
 # Run server
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/hello')
 def hello_world():
-    return 'Hello World'
+    return 'Hello World!'
 
 @app.route('/authorizelogin')
 def authorize():
