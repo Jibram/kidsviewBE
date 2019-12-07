9a85c486e51b7d48b42ff3e9e1358cf12d234672
Raj Yuvaraj <ryuvaraj37@gmail.com>
2019-12-06 19:48:29 -0800

    fixed insert patient and added entries in db

--- index.py
+++ index.py
@@ -82,6 +82,6 @@ def post_patient():
 
         db_cursor.execute(insert_query)  
         #print(json.dumps(db_cursor.fetchone()))
-        return 200
+        return 'OK'
 if __name__ == '__main__':
     app.run(port=5000)
